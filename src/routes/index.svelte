55009c7f331e1ecd233b7e0c4b2a4c8a75610619
Akash <bhosaleakash3915@gmail.com>
2022-06-08 01:35:57 +0530

    sveltekit store

---
+++ src/routes/index.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { posts } from '../stores';
+	import { onMount } from 'svelte';
+	const getData = async () => {
+		let res = await fetch('https://jsonplaceholder.typicode.com/posts');
+		res = await res.json();
+		await posts.set(res);
+	};
+	onMount(() => {
+		getData();
+		console.log($posts, 'asasas', 'asasassa', 'sasasassa');
+	});
+</script>
+
+<ul>
+	{#each $posts as post}
+		<li>{post.body}</li>
+	{/each}
+</ul>
